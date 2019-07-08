684d765781e91842bef56451f6f5b02964a3be23
Nitin199721 <nitin.15bit1095@abes.ac.in>
2019-07-08 16:09:02 +0530

    new

---
+++ Converter/DateConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MVVMPraticle.Converter { 
+    public class DateConverter : IValueConverter
+{
+	    public object Convert(object value)
+	    {
+         DateTime date = (DateTime)value;
+            date = DateTime.Today;
+             return date.ToString("dd/mmm/yyyy");
+	    }
+
+        public object ConvertBack(object value)
+        {
+            return value;
+        }
+    }
+}
